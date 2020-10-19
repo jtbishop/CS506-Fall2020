f3264efc173de0816134175cf67f2758817452d5
James Bishop <jtbishop@bu.edu>
2020-10-19 13:10:18 -0400

    kmeans and read

--- 02-library/cs506/read.py
+++ 02-library/cs506/read.py
@@ -3,4 +3,16 @@ def read_csv(csv_file_path):
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    raise NotImplementedError()
+    arr: list = []
+
+    with open(csv_file_path) as csv_file:
+        for i in csv_file.readlines():
+            arr.append(i.split())
+
+
+    print(arr)
+
+
+
+if __name__=='__main__':
+    read_csv("C:/Users/bishj/OneDrive/College Notebook/Boston University/Fall Senior Year/CS 506/CS506-Fall2020/02-library/tests/test_files/dataset_1.csv")
